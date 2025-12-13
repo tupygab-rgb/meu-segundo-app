27e95c1c05c4fc1d933ee1e98b37ff992126e2af
tupygab-rgb <tupygab@gmail.com>
2025-12-12 22:21:55 -0300

    Update Segundoapp.py

--- Segundoapp.py
+++ Segundoapp.py
@@ -1,8 +1,10 @@
+import streamlit as st
 st.subheader("Seção 1")
 st.write("Conteúdo da primeira seção")
 st.divider()  # Linha horizontal
 st.subheader("Seção 2")
 st.write("Conteúdo da segunda seção")
+
 
 
 
